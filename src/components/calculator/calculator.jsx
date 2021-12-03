4ffec391495e731576d6e1068501b6127339207d
Nabeel-Chhipa <58781270+Nabeel-Chhipa@users.noreply.github.com>
2021-12-03 15:47:58 +0500

    update calculator data

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -14,16 +14,7 @@ class Calculator extends Component {
             academicLevel: "",
             urgency: "",
             services: "",
-            // calculator: {
-            //     Academic_paper_writing: 10.50,
-            //     Rewriting: 30.87,
-            //     Math_Physic_Economic_Statistic_Problems: 32.27,
-            //     Proofreading: 27.87,
-            //     Editing: 29.37,
-            //     Copywriting: 35.28,
-            //     pages: 0,
-            //     words: 275,
-            // }
+            secondacademicLevel: "",
         }
     }
 
@@ -45,23 +36,15 @@ class Calculator extends Component {
     removePages = () => {
         this.setState({ pages: this.state.pages - 1 })
         this.setState({ words: this.state.words - 275 })
-
     }
     service = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         const datas = data.filter(number => {
             return number.id === e.target.value;
-            // if (e) {
-            //     return number.id === e.target.value;
-            // }
-            // else {
-            //     return number.id === 1;
-            // }
         })
         // console.log(datas);
         datas.map(item => this.setState({ type: item.type_papers, services: item.price }))
     }
-
 
     componentDidMount = () => {
         const datas = data.filter(number => {
@@ -70,7 +53,6 @@ class Calculator extends Component {
         // console.log(datas);
         datas.map(item => this.setState({ type: item.type_papers, services: item.price }))
     }
-
 
     render() {
 
@@ -95,11 +77,9 @@ class Calculator extends Component {
                         this.state.type.map(item => {
                             return (
                                 <option label={item.name} value={item.price} defaultValue={item.price}>{item.name}</option>
-
                             )
                         })
                     }
-
                 </select>
                 <label for="pages">NUMBER OF PAGES / WORDS</label>
                 <div className="d-flex justify-content-around">
@@ -122,13 +102,19 @@ class Calculator extends Component {
                     <button onClick={this.addWords}>+</button>
                 </div>
                 <br /> */}
-                <label for="customer_order_service_id">ACADEMIC LEVEL</label>
-                <select id="customer_order_service_id" onChange={(e) => this.setState({ academicLevel: e.target.value })}>
+                <label for="academic_level_1">ACADEMIC LEVEL</label>
+                <select id="academic_level_1" onChange={(e) => this.setState({ academicLevel: e.target.value })}>
                     <option label="GCSE / A Level" value="1">GCSE / A Level</option>
                     <option label="HNC / HND" value="21">HNC / HND</option>
                     <option label="Undergraduate" value="50">Undergraduate</option>
                     <option label="Master's" value="35">Master's</option>
                     <option label="Doctoral" value="40">Doctoral</option>
+                </select>
+                <label for="academic_level_2">ACADEMIC LEVEL</label>
+                <select id="academic_level_2" onChange={(e) => this.setState({ secondacademicLevel: e.target.value })}>
+                    <option label="Bachelor's" value="6">Bachelor's</option>
+                    <option label="Master's" value="8">Master's</option>
+                    <option label="Doctoral" value="15">Doctoral</option>
                 </select>
                 <label for="customer_order_service_id">URGENCY</label>
                 <select id="customer_order_service_id" onChange={(e) => this.setState({ urgency: e.target.value })}>
--- src/components/calculator/data.jsx
+++ src/components/calculator/data.jsx
@@ -1,154 +1,479 @@
 const data = [
     {
-    id: "1",
-    name: "Academic paper writing",
-    price: "10",
-    type_papers: [{
-        name: "Essay",
-        price: "20",
-    }, {
-        name: "Assignment",
+        id: "1",
+        name: "Academic paper writing",
         price: "10",
-    }, {
-        name: "Term Paper",
-        price: "10",
-    }, {
-        name: "Research Paper",
-        price: "10",
-    }, {
-        name: "Capstone Project",
-        price: "5,"
-    }, {
-        name: "Coursework",
-        price: "30",
+        type_papers: [{
+            name: "Essay",
+            price: "20",
+        }, {
+            name: "Assignment",
+            price: "10",
+        }, {
+            name: "Term Paper",
+            price: "10",
+        }, {
+            name: "Research Paper",
+            price: "10",
+        }, {
+            name: "Capstone Project",
+            price: "5,"
+        }, {
+            name: "Coursework",
+            price: "30",
+        }, {
+            name: "Outline",
+            price: "10.53",
+        }, {
+            name: "Book report",
+            price: "10.53",
+        }, {
+            name: "Book review",
+            price: "10.53",
+        }, {
+            name: "Movie review",
+            price: "10.53",
+        }, {
+            name: "Critical essay",
+            price: "10.53",
+        }, {
+            name: "Research summary",
+            price: "10.53",
+        }, {
+            name: "Reflective writing",
+            price: "10.53",
+        }, {
+            name: "Thesis",
+            price: "10.53",
+        }, {
+            name: "Discussion board post",
+            price: "10.53",
+        }, {
+            name: "Report",
+            price: "10.53",
+        }, {
+            name: "Case study",
+            price: "10.53",
+        }, {
+            name: "Lab report",
+            price: "10.53",
+        }, {
+            name: "Power Point presentation",
+            price: "10.53",
+        }, {
+            name: "Article",
+            price: "10.53",
+        }, {
+            name: "Article critique",
+            price: "10.53",
+        }, {
+            name: "Annotated bibliography",
+            price: "10.53",
+        }, {
+            name: "Reaction paper",
+            price: "10.53",
+        }, {
+            name: "Project",
+            price: "10.53",
+        }, {
+            name: "Other",
+            price: "10.90",
+        }
+        ]
+    },
+    {
+        id: "2",
+        name: "Dissertation services",
+        price: "14",
+        type_papers: [{
+            name: "Dissertation",
+            price: "14.29",
+        }, {
+            name: "Dissertation/Thesis proposal",
+            price: "14.29",
+        }, {
+            name: "Dissertation Chapter - Abstract",
+            price: "14.29",
+        }, {
+            name: "Dissertation Chapter - Introduction Chapter",
+            price: "14.29",
+        }, {
+            name: "Dissertation Chapter - Literature review",
+            price: "14.29",
+        }, {
+            name: "Dissertation Chapter - Results",
+            price: "14.29",
+        }, {
+            name: "Dissertation Chapter - Discussion",
+            price: "14.29",
+        }, {
+            name: "Formatting",
+            price: "9.02",
+        }, {
+            name: "Editing",
+            price: "9.02",
+        }, {
+            name: "Proofreading",
+            price: "9.02",
+        }, {
+            name: "Power Point presentation",
+            price: "14.29",
+        },
+        ]
+    },
+    {
+        id: "3",
+        name: "Admission Services",
+        price: "11.28",
+        type_papers: [{
+            name: "Personal statement",
+            price: "9.05",
+        }, {
+            name: "Admission essay",
+            price: "9.05",
+        }, {
+            name: "Research proposal",
+            price: "9.05",
+        }, {
+            name: "Formatting",
+            price: "8.27",
+        }, {
+            name: "Editing",
+            price: "8.27",
+        }, {
+            name: "Scholarship essay",
+            price: "11.28",
+        }, {
+            name: "Power Point presentation",
+            price: "11.28",
+        },
+        ]
+    },
+    {
+        id: "4",
+        name: "Math/Physic/Economic/Statistic Problems",
+        price: "6",
+        type_papers: [{
+            name: "Calculation",
+            price: "15.4",
+        }, {
+            name: "Proof",
+            price: "15.4",
+        }, {
+            name: "Research",
+            price: "15.4",
+        }, {
+            name: "Equation",
+            price: "15.4",
+        }, {
+            name: "Optimization",
+            price: "15.4",
+        }, {
+            name: "Math modeling",
+            price: "15.4"
+        }
+        ]
+    },
+    {
+        id: "5",
+        name: "Multiple Choice Questions",
+        price: "3.75",
+        type_papers: [{
+            name: "Multiple Choice Questions (Time-framed)",
+            price: "3.75",
+        }, {
+            name: "Multiple Choice Questions (Non-time-framed)",
+            price: "4.51",
+        }
+        ]
+    },
+    {
+        id: "6",
+        name: "Editing",
+        price: "16",
+        type_papers: [{
+            name: "Essay",
+            price: "20",
+        }, {
+            name: "Assignment",
+            price: "10",
+        }, {
+            name: "Term Paper",
+            price: "10",
+        }, {
+            name: "Research Paper",
+            price: "10",
+        }, {
+            name: "Capstone Project",
+            price: "5,"
+        }, {
+            name: "Coursework",
+            price: "30",
+        }, {
+            name: "Outline",
+            price: "10.53",
+        }, {
+            name: "Book report",
+            price: "10.53",
+        }, {
+            name: "Book review",
+            price: "10.53",
+        }, {
+            name: "Movie review",
+            price: "10.53",
+        }, {
+            name: "Critical essay",
+            price: "10.53",
+        }, {
+            name: "Research summary",
+            price: "10.53",
+        }, {
+            name: "Reflective writing",
+            price: "10.53",
+        }, {
+            name: "Thesis",
+            price: "10.53",
+        }, {
+            name: "Discussion board post",
+            price: "10.53",
+        }, {
+            name: "Report",
+            price: "10.53",
+        }, {
+            name: "Case study",
+            price: "10.53",
+        }, {
+            name: "Lab report",
+            price: "10.53",
+        }, {
+            name: "Power Point presentation",
+            price: "10.53",
+        }, {
+            name: "Article",
+            price: "10.53",
+        }, {
+            name: "Article critique",
+            price: "10.53",
+        }, {
+            name: "Annotated bibliography",
+            price: "10.53",
+        }, {
+            name: "Reaction paper",
+            price: "10.53",
+        }, {
+            name: "Project",
+            price: "10.53",
+        }
+        ]
+    },
+    {
+        id: "7",
+        name: "Proofreading",
+        price: "16",
+        type_papers: [{
+            name: "Essay",
+            price: "20",
+        }, {
+            name: "Assignment",
+            price: "10",
+        }, {
+            name: "Term Paper",
+            price: "10",
+        }, {
+            name: "Research Paper",
+            price: "10",
+        }, {
+            name: "Capstone Project",
+            price: "5,"
+        }, {
+            name: "Coursework",
+            price: "30",
+        }, {
+            name: "Outline",
+            price: "10.53",
+        }, {
+            name: "Book report",
+            price: "10.53",
+        }, {
+            name: "Book review",
+            price: "10.53",
+        }, {
+            name: "Movie review",
+            price: "10.53",
+        }, {
+            name: "Critical essay",
+            price: "10.53",
+        }, {
+            name: "Research summary",
+            price: "10.53",
+        }, {
+            name: "Reflective writing",
+            price: "10.53",
+        }, {
+            name: "Thesis",
+            price: "10.53",
+        }, {
+            name: "Discussion board post",
+            price: "10.53",
+        }, {
+            name: "Report",
+            price: "10.53",
+        }, {
+            name: "Case study",
+            price: "10.53",
+        }, {
+            name: "Lab report",
+            price: "10.53",
+        }, {
+            name: "Power Point presentation",
+            price: "10.53",
+        }, {
+            name: "Article",
+            price: "10.53",
+        }, {
+            name: "Article critique",
+            price: "10.53",
+        }, {
+            name: "Annotated bibliography",
+            price: "10.53",
+        }, {
+            name: "Reaction paper",
+            price: "10.53",
+        }, {
+            name: "Project",
+            price: "10.53",
+        }
+        ]
+    },
+    {
+        id: "8",
+        name: "Rewriting",
+        price: "6",
+        type_papers: [{
+            name: "Essay",
+            price: "20",
+        }, {
+            name: "Assignment",
+            price: "10",
+        }, {
+            name: "Term Paper",
+            price: "10",
+        }, {
+            name: "Research Paper",
+            price: "10",
+        }, {
+            name: "Capstone Project",
+            price: "5,"
+        }, {
+            name: "Coursework",
+            price: "30",
+        }, {
+            name: "Outline",
+            price: "10.53",
+        }, {
+            name: "Book report",
+            price: "10.53",
+        }, {
+            name: "Book review",
+            price: "10.53",
+        }, {
+            name: "Movie review",
+            price: "10.53",
+        }, {
+            name: "Critical essay",
+            price: "10.53",
+        }, {
+            name: "Research summary",
+            price: "10.53",
+        }, {
+            name: "Reflective writing",
+            price: "10.53",
+        }, {
+            name: "Thesis",
+            price: "10.53",
+        }, {
+            name: "Discussion board post",
+            price: "10.53",
+        }, {
+            name: "Report",
+            price: "10.53",
+        }, {
+            name: "Case study",
+            price: "10.53",
+        }, {
+            name: "Lab report",
+            price: "10.53",
+        }, {
+            name: "Power Point presentation",
+            price: "10.53",
+        }, {
+            name: "Article",
+            price: "10.53",
+        }, {
+            name: "Article critique",
+            price: "10.53",
+        }, {
+            name: "Annotated bibliography",
+            price: "10.53",
+        }, {
+            name: "Reaction paper",
+            price: "10.53",
+        }, {
+            name: "Project",
+            price: "10.53",
+        }
+        ]
+    },
+    {
+        id: "9",
+        name: "Copywriting",
+        price: "16",
+        type_papers: [{
+            name: "Press Release",
+            price: "20",
+        }, {
+            name: "Website Content",
+            price: "10",
+        }, {
+            name: "News article",
+            price: "10",
+        }, {
+            name: "Blogpost",
+            price: "10",
+        }, {
+            name: "Website review",
+            price: "5,"
+        }, {
+            name: "Product review",
+            price: "30",
+        }
+        ]
+    },
+    {
+        id: "10",
+        name: "Resume/CV services",
+        price: "12",
+        type_papers: [{
+            name: "Entry",
+            price: "10"
+        }, {
+            name: "Professional",
+            price: "10"
+        }, {
+            name: "Career change",
+            price: "10"
+        }, {
+            name: "Executive",
+            price: "10"
+        }, {
+            name: "Military",
+            price: "10"
+        }, {
+            name: "Federal",
+            price: "10"
+        },
+        ]
     }
-    ]
-},
-{
-    id: "2",
-    name: "Rewriting",
-    price: "6",
-    type_papers: [{
-        name: "Testing",
-        price: "20",
-    }, {
-        name: "Assignment",
-        price: "10",
-    }, {
-        name: "Term Paper",
-        price: "10",
-    }, {
-        name: "Research Paper",
-        price: "10",
-    }, {
-        name: "Book report",
-        price: "5,"
-    }, {
-        name: "Coursework",
-        price: "30",
-    }
-    ]
-},
-{
-    id: "3",
-    name: "Math/Physic/Economic/Statistic Problems",
-    price: "6",
-    type_papers: [{
-        name: "Calculation",
-        price: "20",
-    }, {
-        name: "Proof",
-        price: "10",
-    }, {
-        name: "Equation",
-        price: "10",
-    }, {
-        name: "Optimization",
-        price: "10",
-    }, {
-        name: "Math modeling",
-        price: "5,"
-    }
-    ]
-},
-{
-    id: "4",
-    name: "Proofreading",
-    price: "16",
-    type_papers: [{
-        name: "Essay",
-        price: "20",
-    }, {
-        name: "Assignment",
-        price: "10",
-    }, {
-        name: "Term Paper",
-        price: "10",
-    }, {
-        name: "Research Paper",
-        price: "10",
-    }, {
-        name: "Book report",
-        price: "5,"
-    }, {
-        name: "Coursework",
-        price: "30",
-    }
-    ]
-},
-{
-    id: "5",
-    name: "Editing",
-    price: "16",
-    type_papers: [{
-        name: "Essay",
-        price: "20",
-    }, {
-        name: "Assignment",
-        price: "10",
-    }, {
-        name: "Term Paper",
-        price: "10",
-    }, {
-        name: "Research Paper",
-        price: "10",
-    }, {
-        name: "Book report",
-        price: "5,"
-    }, {
-        name: "Coursework",
-        price: "30",
-    }
-    ]
-},
-{
-    id: "6",
-    name: "Copywriting",
-    price: "16",
-    type_papers: [{
-        name: "Press Release",
-        price: "20",
-    }, {
-        name: "Website Content",
-        price: "10",
-    }, {
-        name: "News article",
-        price: "10",
-    }, {
-        name: "Blogpost",
-        price: "10",
-    }, {
-        name: "Website review",
-        price: "5,"
-    }, {
-        name: "Product review",
-        price: "30",
-    }
-    ]
-},
-
 ]
-
-
 
 export default data;
