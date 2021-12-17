51441e8c90aa43113ba274961e6017e259bf0024
HamZa AbbaSi <68690482+abbasi440@users.noreply.github.com>
2021-12-17 14:47:52 -0800

    base url

--- src/components/Apiurl/apiurl.js
+++ src/components/Apiurl/apiurl.js
@@ -1,3 +1,4 @@
 import React, { Component } from 'react'
 
-export const baseurl ="http://192.168.0.103/api"
+export const baseurl = "http://192.168.18.2/api"
+export const allurl = "http://192.168.18.2/"
